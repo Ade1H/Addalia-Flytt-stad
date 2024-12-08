1627b7e15fce9d782c2d4a9ee4ce01ead4cbb88e
Ade1H <adel.hosseini@iths.se>
2024-12-08 02:44:55 +0100

    fixa navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,22 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-transparent fixed-top" style={{ backgroundColor: "#ced4da", zIndex: 10  }}>
+    <nav
+      className="navbar navbar-expand-lg fixed-top"
+      style={{ backgroundColor: "#ced4da", zIndex: 10, opacity:0.7 }}
+    >
       <div className="container-fluid">
+        {/* Logo */}
+        <Link to="/" className="navbar-brand">
+          <img
+            src="/Addalia-5.png"
+            alt="Logo"
+            className="rounded-circle"
+            style={{ width: "100px", height: "100px", opacity: 0.8 }}
+          />
+        </Link>
 
-
+        {/* Toggler Button */}
         <button
   className="navbar-toggler"
   type="button"
@@ -15,99 +27,54 @@ const Navbar = () => {
   aria-controls="navbarNav"
   aria-expanded="false"
   aria-label="Toggle navigation"
-  style={{
-    border: "none", // Ingen kant runt knappen
-  }}
 >
-  <span
-    className="navbar-toggler-icon"
-    style={{
-      filter: "invert(1)", // Ändrar färgen till vit
-    }}
-  ></span>
+  <span className="navbar-toggler-icon"></span>
 </button>
 
-<Link to="/" className="navbar-brand">
-          <img
-            src="/Addalia-5.png"
-            alt="Logo"
-            className="rounded-circle"
-            style={{ width: "100px", height: "100px", opacity: 0.6   }}
-          />
-        </Link>
-<div className="collapse navbar-collapse" id="navbarNav">
-  <ul className="navbar-nav"   style={{
-          backgroundColor: "#fff", // Vit bakgrund
-          color: "#000", // Svart text
-          borderRadius: "5px", // Rundade hörn
-          padding: "10px 15px", // Luft runt texten
-          border: "1px solid rgba(0, 0, 0, 0.1)", // Lätt kantlinje
-          textDecoration: "none", // Ingen understrykning
-          display: "inline-block", // Hindrar knappen från att ta hela bredden
-        }}>
-    {/* Tjänster Dropdown */}
-    <li className="nav-item dropdown">
-      <button
-        className="nav-link dropdown-toggle"
-        id="servicesDropdown"
-        data-bs-toggle="dropdown"
-        aria-expanded="false"
+        {/* Navbar Links */}
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            {/* Tjänster Dropdown */}
+            <li className="nav-item dropdown">
+              <button
+                className="nav-link dropdown-toggle btn btn-link"
+                id="servicesDropdown"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                TJÄNSTER
+              </button>
+              <ul className="dropdown-menu" aria-labelledby="servicesDropdown">
+                <li>
+                  <Link to="/stad" className="dropdown-item">
+                    Städtjänster
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/flytt" className="dropdown-item">
+                    Flyttjänster
+                  </Link>
+                </li>
+              </ul>
+            </li>
 
-      >
-        TJÄNSTER
-      </button>
-      <ul
-        className="dropdown-menu"
-        aria-labelledby="servicesDropdown"
+            {/* Offertförfrågan */}
+            <li className="nav-item">
+              <Link to="/offertforfragan" className="nav-link">
+                OFFERTFÖRFRÅGA
+              </Link>
+            </li>
 
-      >
-        <li>
-          <Link
-            to="/stad"
-            className="dropdown-item"
-
-          >
-            Städtjänster
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/flytt"
-            className="dropdown-item"
-
-          >
-            Flyttjänster
-          </Link>
-        </li>
-      </ul>
-    </li>
-
-    {/* Om Oss */}
-    <li className="nav-item">
-      <Link
-        to="/offertforfragan"
-        className="nav-link"
-
-      >
-         OFFERTFÖRFRÅGA
-      </Link>
-    </li>
-    <li className="nav-item">
-      <Link
-        to="/OmOss"
-        className="nav-link"
-
-      >
-        OM OSS
-      </Link>
-    </li>
-  </ul>
-</div>
-
-
+            {/* Om Oss */}
+            <li className="nav-item">
+              <Link to="/OmOss" className="nav-link">
+                OM OSS
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
-
   );
 };
 
